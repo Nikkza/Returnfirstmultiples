c21054b555b2955fd253236bf5155d8192c508db
Nikkza <isaksson.nicklas80@outlook.com>
2020-05-06 23:05:34 +0300

    Add project files.

---
+++ Returnfirstmultiples/Logic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnFirstMultiples
+{
+    public class Logic
+    {
+        public List<Tuple<int, double>> Parser(string yint, string xdouble)
+        {
+            var mylist = new List<Tuple<int, double>>();
+            bool tryToParse;
+            int intNumber = 0;
+            double dNumber = 0;
+            try
+            {
+                tryToParse = double.TryParse(xdouble, NumberStyles.Number, CultureInfo.GetCultureInfo("en-US"), out dNumber)
+                    && Int32.TryParse(yint, out intNumber);
+                if (tryToParse)
+                    mylist.Add(Tuple.Create(intNumber, dNumber));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return mylist;
+        }
+
+        public double[] Multiples(int m, double n)
+        {
+            double[] array = { };
+            for (int i = 1; i <= m; i++)
+                array = array.Concat(new double[] { n * i }).ToArray();
+            return array;
+        }
+    }
+}
---
+++ Returnfirstmultiples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnFirstMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic _logic = new Logic();
+            string yInt = string.Empty;
+            string xDouble = string.Empty;
+            while (true)
+            {
+                yInt = Console.ReadLine();
+                xDouble = Console.ReadLine();
+                var list = _logic.Parser(yInt, xDouble);
+                var results = _logic.Multiples(list[0].Item1, list[0].Item2);
+                Console.WriteLine($"Multiples: {string.Join(",", results)}");
+                Console.WriteLine("-----------------");
+
+            }
+        }
+    }
+}
